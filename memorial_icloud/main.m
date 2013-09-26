e07f668fc5be3fc50046d4a08bc168de0b7dc64a
Colin Yang Hong <colinyang@nmg.com.hk>
2013-09-26 11:17:34 +0800

    Initial Commit

---
+++ memorial_icloud/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  memorial_icloud
+//
+//  Created by Colin Yang Hong on 13-9-26.
+//  Copyright (c) 2013年 Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
