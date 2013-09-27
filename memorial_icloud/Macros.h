63cea7a24e1b29c296e31e7516727b1d4749c14e
Colin Yang Hong <colinyang@nmg.com.hk>
2013-09-27 10:46:43 +0800

    添加一些关于系统版本的宏

---
+++ memorial_icloud/Macros.h
@@ -0,0 +1,19 @@
+//
+//  Macros.h
+//  memorial_icloud
+//
+//  Created by Colin Yang Hong on 13-9-27.
+//  Copyright (c) 2013年 Yang Hong. All rights reserved.
+//
+
+#ifndef memorial_icloud_Macros_h
+#define memorial_icloud_Macros_h
+
+#define IOS_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define IOS_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define IOS_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+
+#endif
